c69c09b77f44cff426bf49a826f6fc44ae4ee599
Solomon Okwa <Dikaeinstein@users.noreply.github.com>
2018-09-25 16:45:28 +0100

    chore(eslint): Setup linting (#4)
    - Installed eslint and necessary config and plugin packages
    - Add .eslintrc config file
    - Renamed the App.js to App.jsx
    - Refactored the App.jsx file to remove eslint warnings
    - Add PR Description Template
    - Add npm script to lint appropriate files
    
    [Finishes #160750598]

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,4 @@
-import React from 'react';
-import { shallow, mount, configure } from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
-
-global.React = React;
-global.shallow = shallow;
-global.mount = mount;
-global.toJson = toJson;
